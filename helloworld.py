d2977eb8e64c29665ca916e297cc409a8a47b6e9
Zsuzsanna Pal <pal.zsuzsanna@gmail.com>
2020-09-17 20:26:06 +0200

    minden commit

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+def print_hello():
+    print("Hello World!")
+
+
+def get_hello_message():
+    return print_hello()
+
+print(get_hello_message())
+
+
+def hello_input():
+    print("What's yout name?")
+    input.__name__
+    else:
+        print("Hello,World!")
+
+
+def helloargument():
+    print("What's your name?)
+
+helloargument()
+
